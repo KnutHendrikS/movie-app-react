3ec224d75705a1b9d2752cf7c17884689db1fff4
KnutHendrikS <70657338+KnutHendrikS@users.noreply.github.com>
2021-01-18 18:24:40 +0200

    MovieList component, movies now showing in browser

---
+++ src/components/MovieList.js
@@ -0,0 +1,13 @@
+const MovieList = ({ movies }) => {
+    return (
+        <>
+            {movies.map((movie, index) => 
+                <div>
+                    <img src={movie.Poster} alt="movie" />
+                </div>
+            )}
+        </>
+    );
+}
+ 
+export default MovieList;
